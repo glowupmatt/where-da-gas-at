acf884bbe4c57e9bee087c6ac1e3763c82e1b428
Sqhy4445 <nchen093@gmail.com>
2024-12-20 12:10:43 -0500

    review test route done

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,76 +1,62 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, request
 from flask_login import login_required, current_user
-from ..models import db, Review
-from ..forms import ReviewForm
+from app.models import db, Review
+
 
 # establish a blueprint for review routes
 review_routes = Blueprint("review", __name__)
 
 
-# #Create Review
+# #Create Review(login user require)
 @review_routes.route("/", methods=["POST"])
 @login_required
 def create_review():
-    created_form = ReviewForm()
-    created_form["csrf_token"].data = request.cookies["csrf_token"]
-    if created_form.validate_on_submit():
-        station_id = (created_form.data.get["station_id"],)
-        review = created_form.data.get["review"]
+    
+    data = request.get_json()
+    required_fields = [
+        column.name
+        for column in Review.__table__.columns
+        if not column.nullable
+        and column.name not in ["id", "user_id"]
+    ]
 
-        # TODO: return only messages that apply
-        if not review or station_id:
-            return (
-                jsonify(
-                    {
-                        "message": "bad request",
-                        "error": {
-                            "review": "review is required",
-                            "station_id": "station id is required",
-                            "user_id": "user id is required",
-                        },
-                    }
-                ),
-                400,
-            )
-
-        review = Review(
-            station_id=station_id,
-            user_id=current_user.id,
-            review=review,
-        )
-
-        db.session.add(review)
-        db.session.commit()
-
-        return {"review": {str(review.id): review.to_dict()}}, 200
+    missing_fields = [
+        field for field in required_fields if field not in data
+    ]
+    if missing_fields:
+        return {
+            "error": f"missing required fields: {missing_fields}"
+        }, 400
 
 
-# Read one review
-@review_routes.route("/<int:review_id>", methods=["GET"])
-def read_review(review_id):
-    try:
-        review = Review.query.filter(Review.id == review_id).first()
+    review = Review(
+        station_id=data["station_id"],
+        user_id=current_user.id,
+        review=data["review"],
+    )
 
-        # failure
-        if not review:
-            return {"error": f"review {review.id} not found"}, 404
+    db.session.add(review)
+    db.session.commit()
 
-        return {"review": {str(review.id): review.to_dict()}}, 200
-
-    except Exception as e:
-        return {"error": str(e)}, 500
-
+    return {"review": {review.id: review.to_dict()}}
 
 # Read All Review
 @review_routes.route("/", methods=["GET"])
 def read_reviews():
     try:
         reviews = Review.query.all()
-        return {
-            "review": {
-                str(review.id): review.to_dict() for reviwe in reviews
-            }
-        }, 200
+        return {"review": {review.id: review.to_dict() for review in reviews}}
+
+    except Exception as e:
+        return {"error": str(e)}, 500
+    
+
+# Read one review by review id
+@review_routes.route("/<int:review_id>", methods=["GET"])
+def read_review(review_id):
+    try:
+        review = Review.query.get(review_id)
+        return {'review': {review.id: review.to_dict()}}
 
     except Exception as e:
         return {"error": str(e)}, 500
@@ -81,46 +67,22 @@ def read_reviews():
 @login_required
 def update_review(review_id):
     try:
-        updated_form = ReviewForm()
-        updated_form["csrf_token"].data = request.cookies[
-            "csrf_token"
-        ]
+        user_id = current_user.id
+        review = Review.query.get(review_id)
 
-        if updated_form.validate_on_submit():
-            review = Review.query.filter(
-                Review.id == review_id
-            ).first()
-
-        # failure
-        if not review:
-            # TODO: return only messages that apply
-            return (
-                jsonify(
-                    {
-                        "message": "bad request",
-                        "error": {
-                            "review": "review is required",
-                            "station_id": "station id is required",
-                            "user_id": f"{review.id}",
-                        },
-                    }
-                ),
-                400,
-            )
-
-        # unathorized
-        if not current_user.id == review.user_id:
-            return {"error": {"message": "Unauthorized"}}, 401
-
-        review.user_id = updated_form.data["user_id"]
-        review.station_id = updated_form.data["station_id"]
-        review.review = updated_form.data["review"]
+        #unathorized
+        if not review.user_id == user_id:
+            return {"message": "forbidden"}, 403
 
         # change to the database
+        data = request.get_json()
+        for key, value in data.items():
+            setattr(review, key, value)
+        
         db.session.commit()
 
         # success
-        return jsonify(review.to_dict()), 200
+        return {"review": {str(review_id): review.to_dict()}}
 
     except Exception as e:
         return {"error": str(e)}, 500
@@ -131,15 +93,17 @@ def update_review(review_id):
 @login_required
 def deleted_review(review_id):
     try:
-        review = Review.query.filter(Review.id == review_id).one()
-
-        # unathorized
-        if not current_user.id == review.user_id:
-            return {"error": {"message": "Unauthorized"}}, 401
+        user_id = current_user.id
+        review = Review.query.get(review_id)
 
         # failure
         if not review:
-            return {"error": f"review {review.id} is not found"}
+            return {"message" "review not found"}
+        
+        # unathorized
+        if not review.user.id == user_id:
+            return {"message": "Unauthorized"}, 401
+
 
         db.session.delete(review)
         db.session.commit()
@@ -147,7 +111,7 @@ def deleted_review(review_id):
         # success
         return {
             "message": f"deleted review {review.id} successfully",
-        }, 200
+        }
 
     except Exception as e:
         return {"error": str(e)}, 500
